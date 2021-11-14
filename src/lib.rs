bce324d1081a19642f1a0b3fef532872342cc5be
aeon <jerry73204@gmail>
2021-11-14 13:54:31 +0800

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,22 @@
 //! The snipplet serializes a compound type of [Tensor], [Kind] and [Device].
 //!
 //! ``` rust
-//! use tch::{Tensor, Device, Kind, Reduction};
+//! use tch::{Device, Kind, Reduction, Tensor};
 //!
 //! #[derive(Debug, serde::Serialize, serde::Deserialize)]
 //! struct Example {
 //!     #[serde(with = "tch_serde::serde_tensor")]
 //!     tensor: Tensor,
-//!         #[serde(with = "tch_serde::serde_kind")]
+//!     #[serde(with = "tch_serde::serde_kind")]
 //!     kind: Kind,
-//!         #[serde(with = "tch_serde::serde_device")]
+//!     #[serde(with = "tch_serde::serde_device")]
 //!     device: Device,
-//!         #[serde(with = "tch_serde::serde_reduction")]
+//!     #[serde(with = "tch_serde::serde_reduction")]
 //!     reduction: Reduction,
 //! }
 //!
 //! let example = Example {
-//!     tensor: Tensor::randn(
-//!         &[2, 3],
-//!         (Kind::Float, Device::Cuda(0)),
-//!     ),
+//!     tensor: Tensor::randn(&[2, 3], (Kind::Float, Device::Cuda(0))),
 //!     kind: Kind::Float,
 //!     device: Device::Cpu,
 //!     reduction: Reduction::Mean,
